d2c552a6446f9b10607b290f5faaa7dd75267649
Irving-8man <geyler0502@outlook.com>
2024-08-19 11:46:23 -0600

    agregado la version para navegador

--- src/dashboard/Plantillas/CrearPlantilla.tsx
+++ src/dashboard/Plantillas/CrearPlantilla.tsx
@@ -6,14 +6,15 @@ export default function CrearPlantilla() {
     return (
         <div>
             <h1>Crear plantilla</h1>
-            <GenerateAndSaveTPL />
+            <GenerateAndSaveTPLDesktop />
+            <GenerateAndDownloadTPLBrowser />
         </div>
 
     )
 }
 
 
-function GenerateAndSaveTPL() {
+function GenerateAndSaveTPLDesktop() {
     const generateFile = async () => {
         const data = {
             // Aquí coloca la estructura JSON que deseas guardar
@@ -53,8 +54,48 @@ function GenerateAndSaveTPL() {
 
     return (
         <button onClick={generateFile}>
-            Generar y guardar archivo .tpl
+            Generar y guardar archivo .tpl desktop
         </button>
     );
 }
+
+
+
+
+function GenerateAndDownloadTPLBrowser() {
+    const generateFile = () => {
+        const data = {
+            // Aquí coloca la estructura JSON que deseas guardar
+            nombre: "Instrumento 1",
+            descripcion: "Descripción del instrumento",
+            preguntas: [
+                { pregunta: "¿Pregunta 1?", respuesta: "Sí" },
+                { pregunta: "¿Pregunta 2?", respuesta: "No" },
+            ]
+        };
+
+        // Convertir el JSON a un string
+        const jsonString = JSON.stringify(data, null, 2);
+
+        // Crear un blob con el contenido del archivo
+        const blob = new Blob([jsonString], { type: 'application/json' });
+
+        // Crear un enlace temporal para descargar el archivo
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'instrumento2.tpl';
+        document.body.appendChild(link);
+        link.click();
+
+        // Limpiar el enlace temporal
+        document.body.removeChild(link);
+    };
+
+    return (
+        <button onClick={generateFile}>
+            Generar y descargar archivo .tpl browser
+        </button>
+    );
+}
+
 
