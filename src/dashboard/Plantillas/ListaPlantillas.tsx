fd7309c5cb93b7f5e12b44cb785d394d05767f66
DidierTec <andreyesquivel24@gmail.com>
2024-08-26 10:20:53 -0600

    UPDATE:PRUEBA

--- src/dashboard/Plantillas/ListaPlantillas.tsx
+++ src/dashboard/Plantillas/ListaPlantillas.tsx
@@ -1,7 +1,7 @@
 export default function ListaPlantillas() {
     return(
         <div>
-             <h1>Lista Plantillas</h1>
+             <h1>Lista Plantillas .l.</h1>
         </div>
        
     
