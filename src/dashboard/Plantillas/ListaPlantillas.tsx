2e9a17487a01697e3d6882bdbc4796c9c4460ea6
JuanConde33 <conde.juan12gmail.com>
2024-08-26 18:03:46 -0600

    update

--- src/dashboard/Plantillas/ListaPlantillas.tsx
+++ src/dashboard/Plantillas/ListaPlantillas.tsx
@@ -1,7 +1,7 @@
 export default function ListaPlantillas() {
     return(
         <div>
-             <h1>Lista Plantillas</h1>
+             <h1>Lista Plantillas 2</h1>
         </div>
        
     
